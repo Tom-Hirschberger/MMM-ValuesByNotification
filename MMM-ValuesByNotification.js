ea212fb3ddb8c0a01fe3f6444f43b79b977025d7
Tom Hirschberger <github@hirschberger.eu>
2022-08-30 08:52:05 +0200

    started implementing the start routine

--- MMM-ValuesByNotification.js
+++ MMM-ValuesByNotification.js
@@ -1,3 +1,5 @@
+const { configs } = require("eslint-plugin-prettier")
+
 /* global Module
 
 /* Magic Mirror²
@@ -10,6 +12,7 @@ Module.register('MMM-ValuesByNotification', {
 
   defaults: {
     positions: "tivu",
+    reuseCount: 1,
     title: null,
     icon: null,
     valueUnit: null,
@@ -40,10 +43,51 @@ Module.register('MMM-ValuesByNotification', {
 
   start: function () {
     const self = this
+    self.notificationPrefix = "VALUES_BY_NOTIFICATION_"
+    self.currentProfile = null
+    self.sensorsSortedByNotification = {}
+    
+    //building a structure which holds the current values based on the notification
+    //this way we can set the values directly if an notification is received rather than
+    //iterating through all groups and items each time
+    for (let groupIdx = 0; i < self.config.groups.length; groupIdx++) {
+      let curGroup = config.groups[groupIdx]
+      for (let itemIdx = 0; i < curGroup.items.length; itemIdx++) {
+        let curItem = curGroup.items[itemIdx]
+        let curNotifcation = curItem.notification
+        
+        //either use global reuse value or the one set for this item
+        if (curItem.has("reuseCount")){
+          let curReuseCount = curItem["reuseCount"]
+        } else {
+          let curReuseCount = self.config["reuseCount"]
+        }
+        
+        if (self.sensorsSortedByNotification.has(self.notificationPrefix+curNotifcation)){
+          let curElement = self.sensorsSortedByNotification[self.notificationPrefix+curNotifcation]
+        } else {
+          let curElement = []
+        }
+        curElement.append(
+          {
+            "groupIdx":groupIdx,
+            "itemIdx": itemIdx,
+            "reuseCount": curReuseCount,
+            "currentUses": curReuseCount,
+            "currentValue": "na"
+          }
+        )
+
+        self.sensorsSortedByNotification[self.notificationPrefix+curNotifcation] = curElement
+      }
+    }
   },
 
   notificationReceived: function (notification, payload) {
     const self = this
+    if (notification === "CHANGED_PROFILE") {
+      self.currentProfile = payload.to
+    }
   },
 
   socketNotificationReceived: function (notification, payload) {
